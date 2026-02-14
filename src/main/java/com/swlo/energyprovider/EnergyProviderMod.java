fc4b9f7f935254109c6d63305552f79ee0b41843
SwloBr <ppaulo2003.ppsr@gmail.com>
2026-02-14 14:43:12 -0300

    feat: Add energy provider creative tab and update localization

--- src/main/java/com/swlo/energyprovider/EnergyProviderMod.java
+++ src/main/java/com/swlo/energyprovider/EnergyProviderMod.java
@@ -1,6 +1,7 @@
 package com.swlo.energyprovider;
 
 import com.swlo.energyprovider.block.ModBlocks;
+import com.swlo.energyprovider.item.ModCreativeModeTabs;
 import com.swlo.energyprovider.item.ModItems;
 import org.slf4j.Logger;
 
@@ -49,6 +50,8 @@ public class EnergyProviderMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+        ModCreativeModeTabs.register(modEventBus);
+
         ModBlocks.register(modEventBus);
         ModItems.register(modEventBus);
 
